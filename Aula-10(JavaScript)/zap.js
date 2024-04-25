7c52cd4349d35bf1f1c5e823bb9b2c2d5e5b7b15
Boris Philipp <129879125+BorisPhilipp@users.noreply.github.com>
2024-04-24 21:04:20 -0300

    Add files via upload

---
+++ Aula-10(JavaScript)/zap.js
@@ -0,0 +1,50 @@
+/*1) Escreva um programa em JavaScript que exiba na tela o maior número
+entre dois números fornecidos pelo usuário.*/
+
+/* const numero1 = +prompt("Digite o Primeiro Numero.");
+const numero2 = +prompt("Digite o Segundo Numero.");
+
+if(numero1 > numero2){
+    console.log("Número: " + numero1 + " é maior que: " + numero2);
+}else if(numero1==numero2){
+    console.log("Os números são iguais.");
+}else{
+    console.log("L");
+} */
+
+
+/*3) Escreva um programa em JavaScript que exiba na tela uma mensagem
+de acordo com a nota do usuário. Se a nota for menor do que 6, exiba
+"Reprovado". Se a nota for maior ou igual a 6 e menor do que 8, exiba
+"Aprovado". Se a nota for maior ou igual a 8, exiba "Aprovado com louvor".*/
+
+
+/* const nota = +prompt("Digite sua Nota.");
+
+if(nota>=8){
+    alert("Aprovado com Louvor");
+}else if(nota>=6){
+    alert("APROVADO")
+}else if(nota<=8){
+    alert("Reprovado")
+} */
+
+
+/*4) Escreva um programa em JavaScript que exiba na tela a média dos
+números de um vetor.*/
+
+/* const vector = [1,2,3];
+let soma = 0;
+for(let i=0; i < vector.length; i++){
+    soma+= vector[i];
+}
+const media = soma / vector.length;
+alert("A média é " + media); */
+
+
+/*5) Escreva um programa em JavaScript que exiba na tela uma mensagem
+de acordo com o valor de uma variável. Se a variável for igual a "a", exiba
+"A". Se a variável for igual a "b", exiba "B". Se a variável não for nem "a"
+nem "b", exiba "Outra letra".*/
+
+
